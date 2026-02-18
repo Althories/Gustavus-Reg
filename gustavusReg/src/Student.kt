b9fa468daf9873c6736005881f01c05b84a6a051
cooperptacek <cooperptacek@gmail.com>
2026-02-17 19:03:39 -0600

    Updated Student.kt
    All specifications from the UML Diagram for Student are now implemented. Enroll course and drop course both rely on the course class being implemented so no testing has been completed yet

--- gustavusReg/src/Student.kt
+++ gustavusReg/src/Student.kt
@@ -1,2 +1,40 @@
-class Student(userID: Int): User(userID) {
-} //comment
+//Cooper Ptacek
+
+class Student(userId: Int) : User(userId) {
+
+    //Initializers
+    var gpa: Float = 0.0f
+    var year: Int = 1
+    var graduationDate: Int = 2030
+    var classes = mutableListOf<Int>()
+    var degrees = mutableListOf<Int>()
+
+    //Course related functions
+    fun enrollCourse(courseId: Int): Boolean {
+        if (classes.contains(courseId))
+            throw IllegalStateException("Already enrolled in this course dummy")
+        // check capacity
+        if (course.students.size >= course.capacity)
+            throw IllegalStateException("U too slow, course is full.")
+        // updates
+        classes.add(courseId)
+        course.students.add(this.userID)
+        return true
+    }
+
+    fun dropCourse(courseId: Int): Boolean {
+        if (!classes.contains(courseId))
+            throw IllegalStateException("U are not even in this course")
+        classes.remove(courseId)
+        course.students.remove(this.userID)
+        return true
+    }
+
+    //Degree Related functions
+    fun declareDegree(degreeId: Int): Boolean {
+        if (degrees.contains(degreeId))
+            return false
+        degrees.add(degreeId)
+        return true
+    }
+}
