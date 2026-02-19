ad9becb758b1bb2dd52d22add5720379360403ed
Reedspun <reedspun@gmail.com>
2026-02-19 12:54:30 -0600

    Updated module dir and added file credit
    This makes it so that I don't have to configure the SDK every time i sync with origin. Probably.

--- gustavusReg/src/Professor.kt
+++ gustavusReg/src/Professor.kt
@@ -1,3 +1,5 @@
+// Branden Hopper
+
 class Professor(userID: Int, var courses: IntArray, var officeLocation: String): User(userID) {
     fun updateCourseDescription(courseID: Int) {
         return
