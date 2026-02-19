cb511fc59ffab5c10c4900fa61eb2bdf234a20f1
Soampbar <Althories.bar@gmail.com>
2026-02-19 13:00:20 -0600

    Commit!
    Main Commit

--- gustavusReg/src/Main.kt
+++ gustavusReg/src/Main.kt
@@ -2,12 +2,14 @@
 fun main() {
     var a = User(0)
     var b = Admin(1)
-    var c = Professor(2, intArrayOf(1, 2, 3, 4), "Olin")
+    //var c = Professor(2, intArrayOf(1, 2, 3, 4), "Olin")
     println(a)
     println(b)
-    println(c)
+    //println(c)
 
     //data globals
-    var users = arrayOf
-
+    var users : MutableList<String> = mutableListOf()
+    var courses : MutableList<String> = mutableListOf()
+    var degrees : MutableList<String> = mutableListOf()
+    var departments : MutableList<String> = mutableListOf()
 }
