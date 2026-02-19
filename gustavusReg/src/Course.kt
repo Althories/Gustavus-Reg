c4fa8211a1167c18feb50d8597ff1f9772f0ef79
jsolhjem <132086500+jsolhjem@users.noreply.github.com>
2026-02-19 12:38:03 -0600

    more course
    still working

--- gustavusReg/src/Course.kt
+++ gustavusReg/src/Course.kt
@@ -1,13 +1,30 @@
-class Course {
-    class Course(val courseId : Int){
+ class Course(val courseId : Int){
         var title : String = "NA"
         var credits: int = 4
         var days : bool[] = [false,false,false,false,false]
-        var time : int = 0
-        var departmentId : int = 0
+        var time : int = -1
+        var departmentId : int = -1
         var description : str ="NA"
         var students  : int[] = []
-        var capacity : int = 0
+        var capacity : int = -1
         var location : str  = "NA"
+
+        fun updateAtributes(
+            title : String = "NA"
+            credits: int = -1
+            days : bool[] = [false,false,false,false,false]
+            time : int = -1
+            departmentId : int = -1
+            description : str ="NA"
+            capacity : int = 0
+            location : str  = "NA"){
+            if(title != "NA"){
+                this.title = title
+            }
+        }
     }
+
+
+fun main(){
+
 }
